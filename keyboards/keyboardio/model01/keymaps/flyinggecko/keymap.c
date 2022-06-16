58db978add113bcbb7931216b43f4991ced0f1a0
flyinggecko <julian@flyinggecko.org>
2022-06-16 12:10:18 +0200

    remove led stuff for model01

--- keyboards/keyboardio/model01/keymaps/flyinggecko/keymap.c
+++ keyboards/keyboardio/model01/keymaps/flyinggecko/keymap.c
@@ -26,7 +26,7 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [EN] = LAYOUT(
   KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(WORKMAN),
-  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , RGB_MOD,    _______, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_EQL ,
+  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    _______, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_EQL ,
   KC_BSPC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_TAB ,    KC_ENT , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
   KC_ESC , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ESC ,    KC_RALT, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
                                          OSM(MOD_RALT),                           KC_LALT,
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 [WORKMAN] = LAYOUT(
   KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(EN),
-  KC_TAB , KC_Q   , KC_D   , KC_R   , KC_W   , KC_B   , RGB_MOD,    _______, KC_J   , KC_F   , KC_U   , KC_P   , KC_SCLN, KC_EQL ,
+  KC_TAB , KC_Q   , KC_D   , KC_R   , KC_W   , KC_B   , _______,    _______, KC_J   , KC_F   , KC_U   , KC_P   , KC_SCLN, KC_EQL ,
   KC_BSPC, KC_A   , KC_S   , KC_H   , KC_T   , KC_G   , KC_TAB ,    KC_ENT , KC_Y   , KC_N   , KC_E   , KC_O   , KC_I   , KC_QUOT,
   KC_ESC , KC_Z   , KC_X   , KC_M   , KC_C   , KC_V   , KC_ESC ,    KC_RALT, KC_K   , KC_L   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
                                          OSM(MOD_RALT),                           KC_LALT,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 [FUN] = LAYOUT(
   RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-  KC_HOME, _______, KC_MS_U, KC_WH_U, KC_BTN2, _______, RGB_TOG,    KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12 ,
+  KC_HOME, _______, KC_MS_U, KC_WH_U, KC_BTN2, _______, _______,    KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12 ,
   KC_DEL , KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, _______, _______,    KC_MPLY, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PGDN, KC_PGUP,
   KC_END , KC_PSCR, KC_INS , KC_WH_D, KC_BTN3, _______, _______,    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSLS, KC_PIPE,
                                          _______,                                 _______,
